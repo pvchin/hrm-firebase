9bd715bc9144b132dee0d61491c499c9a5dbed66
pvchine <pvchinbn@yahoo.com>
2022-03-01 08:35:19 +0800

    2022 Mar 01

--- src/components/Export2Excel.js
+++ src/components/Export2Excel.js
@@ -1,31 +1,14 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 //import ReactExport from "react-export-excel";
 import ReactExport from "react-data-export";
 //import { ExcelFile, ExcelSheet } from "react-export-excel";
 import { Button } from "@chakra-ui/react";
 
-
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 //const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
-const months = [
-  { name: "January", value: "1" },
-  { name: "February", value: "2" },
-  { name: "March", value: "3" },
-  { name: "April", value: "4" },
-  { name: "May", value: "5" },
-  { name: "June", value: "6" },
-  { name: "July", value: "7" },
-  { name: "August", value: "8" },
-  { name: "September", value: "9" },
-  { name: "October", value: "10" },
-  { name: "November", value: "11" },
-  { name: "December", value: "12" },
-];
-
-const Export2Excel = ({ filename, dataset , title }) => {
-  
+const Export2Excel = ({ filename, dataset, title }) => {
   const hoccols = [
     {
       title: "What is/are the findings?",
@@ -405,7 +388,7 @@ const Export2Excel = ({ filename, dataset , title }) => {
       data: hocdata,
     },
   ];
- 
+
   return (
     // <ExcelFile element={<Button>Export to Excel</Button>} filename={filename}>
     //   <ExcelSheet dataSet={multiDataSet1} name="Organization" />
